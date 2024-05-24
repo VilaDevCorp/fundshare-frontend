506969d00bbf978496de78a071451cf366907e14
david.vila <davilasanvilar@gmail.com>
2024-05-24 16:47:04 +0200

    Button component theme

--- src/theme/ButtonTheme.tsx
+++ src/theme/ButtonTheme.tsx
@@ -2,38 +2,66 @@ import { defineStyleConfig } from '@chakra-ui/react';
 
 export const ButtonTheme = defineStyleConfig({
     // The styles all button have in common
-    baseStyle: {},
-    // Two sizes: sm and md
-    sizes: {},
-    // Two variants: outline and solid
-    variants: {
-        outline: {
-            border: '2px solid',
-            borderColor: 'primary.600',
-            _hover: {},
-            _dark: {
-                color: 'primary.200'
-            },
-            color: 'primary.900'
+    baseStyle: {
+        borderRadius: '2px',
+        borderWidth: '2px !important',
+        _disabled: {
+            cursor: 'default'
         },
+    },
+    sizes: {
+        md: {
+            padding: '8px 16px',
+            fontSize: '1.2em',
+            fontWeight: 'regular',
+            svg: {
+                fontSize: '1.2em',
+            }
+        },
+    },
+    variants: {
         solid: {
-            bg: 'primary.600',
+            bg: 'primary.500',
+            padding: '10px',
+            borderColor: 'transparent',
             _hover: {
-                bg: 'primary.500'
+                bg: 'primary.300'
+            },
+            _active: {
+                bg: 'primary.200',
             },
             _disabled: {
-                bg: 'primary.600 !important'
+                bg: 'primary.100 !important',
             },
-            color: 'white'
+            color: 'background.50'
         },
+        outline: {
+            borderColor: 'primary.500',
+            _hover: {
+                bg: 'background.100',
+            },
+            _active: {
+                bg: 'background.200',
+            },
+            _disabled: {
+                borderColor: 'primary.100',
+                color: 'primary.100'
+            },
+            color: 'primary.500'
+        },
+
         ghost: {
             bg: 'transparent',
+            borderColor: 'transparent',
             _hover: {
-                color: 'primary.900',
-                bg: 'transparent !important'
+                color: 'primary.300',
+                bg: 'background.100'
+            },
+            _active: {
+                bg: 'background.200'
             },
             _disabled: {
-                color: 'primary.50'
+                color: 'primary.100'
             },
             color: 'primary.500'
         }
