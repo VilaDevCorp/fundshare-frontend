ae4c3793de341e006a8ecb373d4f618dd82e082a
david.vila <davilasanvilar@gmail.com>
2024-05-25 20:24:45 +0200

    Fixes in InputTheme

--- src/theme/InputTheme.tsx
+++ src/theme/InputTheme.tsx
@@ -1,29 +1,30 @@
-import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
-import { inputAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react';
+import { inputAnatomy } from '@chakra-ui/anatomy';
 
-const { defineMultiStyleConfig: defineMultiStyleConfigInput } =
-    createMultiStyleConfigHelpers(inputAnatomy.keys);
+const { defineMultiStyleConfig } = createMultiStyleConfigHelpers(
+    inputAnatomy.keys
+);
 
-export const InputTheme = defineMultiStyleConfigInput({
+export const InputTheme = defineMultiStyleConfig({
     baseStyle: {
-
         field: {
             borderRadius: 2,
             color: 'text.500',
-            fontSize: 'sm',
+            fontSize: 'sm'
         },
         addon: {
-            fontSize: 'lg',
-        },
+            fontSize: 'lg'
+        }
     },
     variants: {
         outline: {
             field: {
                 borderColor: 'text.700',
+                padding: '10px',
+                height: '48px',
 
                 _hover: {
-                    boxShadow: 'inputHover',
-
+                    boxShadow: 'inputHover'
                 },
                 _focus: {
                     borderColor: 'primary.500',
@@ -31,29 +32,31 @@ export const InputTheme = defineMultiStyleConfigInput({
                     color: 'text.900',
                     _invalid: {
                         borderColor: 'error.500',
-                        boxShadow: 'inputInvalid',
-                    },
+                        boxShadow: 'inputInvalid'
+                    }
                 },
                 _invalid: {
                     borderColor: 'error.500',
-                    boxShadow: 'inputInvalid',
+                    boxShadow: 'inputInvalid'
                 },
                 _disabled: {
                     _hover: {
                         borderColor: 'background.100',
-                        boxShadow: 'none',
-
+                        boxShadow: 'none'
                     },
                     borderColor: 'background.100',
                     opacity: 1,
                     bg: 'background.100',
-                    cursor: 'default',
-                },
+                    cursor: 'default'
+                }
             },
             addon: {
-            },
-        },
-    },
-
-
+                bg: 'background.50',
+                color: 'text.800',
+                fontSize: 'lg',
+                borderWidth: 0,
+                height: 'auto',
+            }
+        }
+    }
 });
