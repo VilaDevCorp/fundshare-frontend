55d81bdc6053e8e9d77e211b5ffa3391c716f28c
david.vila <davilasanvilar@gmail.com>
2024-05-28 19:55:45 +0200

    Layout fixes

--- src/components/organism/Layout.tsx
+++ src/components/organism/Layout.tsx
@@ -1,7 +1,8 @@
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import logo from '/logo.svg';
-import { Button } from '@chakra-ui/react';
+import { TopMenu } from '../molecule/TopMenu';
+import { BottomNav } from '../molecule/BottomNav';
+import { useScreen } from '../../hooks/useScreen';
 
 export function Layout({
     children,
@@ -11,32 +12,33 @@ export function Layout({
     isPublic?: boolean;
 }) {
     const { user } = useAuth();
-    const navigate = useNavigate();
+    const { isLaptop } = useScreen();
 
     return isPublic || user ? (
         //Public Layout
         isPublic ? (
-            <main className="min-h-full flex-col md:h-auto md:min-h-full w-full p-4 flex h-screen backdrop-blur-sm items-center justify-center">
+            <main className="min-h-full flex-col md:h-auto md:min-h-full w-full p-4 flex backdrop-blur-sm items-center justify-center overflow-auto">
                 {children}
             </main>
         ) : (
             //Private layout
             <>
-                {/* <Menu /> */}
+                <TopMenu />
                 <main
                     className={`w-full flex flex-col h-full max-w-[500px] m-auto p-8 gap-8`}
                 >
                     {children}
                 </main>
+                {!isLaptop && <BottomNav />}
             </>
         )
     ) : (
         //Not logged in layout
         <div
-            className={`min-h-full flex  flex-col items-center bg-center md:min-h-full h-screen w-full px-4 py-4`}
+            className={`min-h-full flex  flex-col items-center bg-center md:min-h-full h-screen w-full`}
         >
             <main
-                className={`flex p-8 rounded-lg h-full md:h-auto justify-center items-center py flex-col gap-6 m-auto`}
+                className={`flex rounded-lg h-full md:h-auto items-center flex-col gap-6 w-full overflow-auto`}
             >
                 <img
                     src={logo}
