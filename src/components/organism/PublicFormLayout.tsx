4f92026e6b1bb9bc753bf992a2c33008631d521c
david.vila <davilasanvilar@gmail.com>
2024-05-29 02:18:54 +0200

    Public form style changes and onsubmit default canceled

--- src/components/organism/PublicFormLayout.tsx
+++ src/components/organism/PublicFormLayout.tsx
@@ -7,17 +7,23 @@ export function PublicFormLayout({
     title
 }: {
     children: JSX.Element | JSX.Element[];
-    onSubmit: () => void;
+    onSubmit?: () => void;
     title?: string;
 }) {
     return (
         <section
-            className={`flex max-w-[500px] w-full md:p-8 
-            rounded-lg md:h-auto max-h-[900px] m-auto mt-auto ml-auto mr-auto flex-col gap-6 `}
+            className={`flex md:max-w-[500px] w-full p-8 
+            rounded-[2px] md:h-auto md:max-h-[900px] md:m-auto flex-col gap-6 bg-background-0 `}
         >
             <img src={logo} alt="Logo" className=" self-center  w-[200px]" />
             <Typography type="title">{title}</Typography>
-            <form onSubmit={onSubmit} className="flex flex-col gap-6">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit && onSubmit();
+                }}
+                className="flex flex-col gap-6"
+            >
                 {children}
             </form>
         </section>
